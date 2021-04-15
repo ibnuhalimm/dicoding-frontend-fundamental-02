4504619f7fa24cc455a93742efddb0d00dafc138
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-04-15 10:29:40 +0700

    implement update api

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -47,7 +47,23 @@ function main() {
     };
 
     const updateBook = (book) => {
-        // tuliskan kode di sini!
+        const xhr = new XMLHttpRequest();
+
+        xhr.onload = function() {
+            const responseJson = JSON.parse(this.responseText);
+            showResponseMessage(responseJson.message);
+            getBook();
+        };
+
+        xhr.onerror = function() {
+            showResponseMessage();
+        };
+
+        xhr.open('PUT', `${baseUrl}/edit/${book.id}`);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('X-Auth-Token', '12345');
+
+        xhr.send(JSON.stringify(book));
     };
 
     const removeBook = (bookId) => {
